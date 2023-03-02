dbed955ab7e693d6f8de46f8efa45ef56f8b5cae
Ryan Sajulga <sajul004@morris.umn.edu>
2023-03-02 13:46:24 -0600

    Create Food Request Tab on HTML
    Add the a single checkbox for experimenting - Ryan Sajulga

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { FsclientListComponent } from './fsclient/fsclient-list.component';
 import { FsclientProfileComponent } from './fsclient/fsclient-profile.component';
 import { AddFsclientComponent } from './fsclient/add-fsclient.component';
+import { RequestFormComponent } from './request-form/request-form.component';
 
 // Note that the 'fsclients/new' route needs to come before 'fsclients/:id'.
 // If 'fsclients/:id' came first, it would accidentally catch requests to
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'fsclients', component: FsclientListComponent, title: 'Fsclients'},
   {path: 'fsclients/new', component: AddFsclientComponent, title: 'Add Fsclient'},
-  {path: 'fsclients/:id', component: FsclientProfileComponent, title: 'Fsclient Profile'}
+  {path: 'fsclients/:id', component: FsclientProfileComponent, title: 'Fsclient Profile'},
+  {path: 'request-forms', component: RequestFormComponent, title: 'Request Form'}
 ];
 
 @NgModule({
